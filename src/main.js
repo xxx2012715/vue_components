14d2c9f9db7304ddcc0ee9da004297acbad1f02d
kite <943443868@qq.com>
2019-11-05 18:55:30 +0800

    bug fix

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
@@ -12,14 +14,8 @@ import './assets/res/css/reset.css'
 
 
 
-
-
-
-
-
-
-
 Vue.config.productionTip = false
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
